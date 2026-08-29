a490053237a93d48baf2a8066109d88d4a31c807
dsvl0 <savelijln0@gmail.com>
2026-08-29 23:21:27 +0400

    ...

--- TelegramInteractions/Commands/forgetme.cs
+++ TelegramInteractions/Commands/forgetme.cs
@@ -12,16 +12,24 @@ namespace Scheder.TelegramInteractions.Commands;
 public class Forgetme : ICommand{
     
     public async Task ExecuteAsync(ITelegramBotClient bot, Message message, string[] args, CancellationToken cancellationToken) {
+        if (message.From is null) return;
+        
         var isGroup = ChatTools.IsGroup(message);
         var threadId = ChatTools.GetForumId(message);
         
         if (isGroup) {
-            await bot.SendMessage(
+            var eMsg = await bot.SendMessage(
                 chatId: message.Chat.Id,
                 text: "Эта команда доступна только в личных чатах",
+                ephemeralMessageParameters: new EphemeralMessageParameters {ReceiverUserId = message.From.Id},
                 messageThreadId: threadId,
                 cancellationToken: cancellationToken
             );
+            
+            await Task.Delay(5000, cancellationToken);
+            
+            await bot.DeleteEphemeralMessage(eMsg.Chat.Id, message.From.Id, (int)eMsg.EphemeralMessageId!, cancellationToken);
+            await bot.DeleteMessage(message.Chat.Id, message.MessageId, cancellationToken);
             
             return;
         }
